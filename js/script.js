424255297a75b50127205fb81ec583bf289fc4ff
dgssdagdg <100527555+dgssdagdg@users.noreply.github.com>
2023-08-02 09:54:43 +0300

    add

--- js/script.js
+++ js/script.js
@@ -49,3 +49,24 @@ window.onscroll = function(){
       Nav.classList.remove('intro-body-nav-flex');
   }
 }
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+  
+        let href = this.getAttribute('href').substring(1);
+  
+        const scrollTarget = document.getElementById(href);
+  
+        const topOffset = document.querySelector('.header-center-nav').offsetHeight;
+        // const topOffset = 0; // если не нужен отступ сверху 
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+  
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+  });
